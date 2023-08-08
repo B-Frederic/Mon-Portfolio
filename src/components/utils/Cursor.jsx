e974f84a515db6b87630b64b33f9a140002cc515
B-Frederic <87258430+B-Frederic@users.noreply.github.com>
2023-08-08 09:24:11 +0200

    patch cursor design

---
+++ src/components/utils/Cursor.jsx
@@ -0,0 +1,44 @@
+import AnimatedCursor from "react-animated-cursor"
+
+function Cursor() {
+  return (
+    <div>
+        {
+            screen.width > 900 ? (
+                <AnimatedCursor
+                innerSize={20}
+                outerSize={20}
+                color='255, 255, 255'
+                innerScale={0.5}
+                outerAlpha={0.5}
+                outerScale={1.5}
+                trailingSpeed={1}
+                clickables={[
+                    'a',
+                    'input[type="text"]',
+                    'input[type="email"]',
+                    'input[type="number"]',
+                    'input[type="submit"]',
+                    'input[type="image"]',
+                    'label[for]',
+                    'select',
+                    'textarea',
+                    'button',
+                    '.link'
+                ]}
+                innerStyle={{
+                    mixBlendMode: "exclusion",
+                }}
+                outerStyle={{
+                    mixBlendMode: 'extenction',
+                    outline: "3px dashed goldenrod"
+                }}
+                />
+            ) : ""
+        }
+
+    </div>
+  )
+}
+
+export default Cursor
